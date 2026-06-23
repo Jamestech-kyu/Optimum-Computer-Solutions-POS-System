ac9e4e777b91166df8c1203eab7115ff9d2451f1
James Nyandere <james.18513@students.kyu.ac.ke>
2026-06-23 15:21:40 +0300

    Fix Render ASGI startup

--- backend/backend/erp_sales/asgi.py
+++ backend/backend/erp_sales/asgi.py
@@ -8,12 +8,13 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "erp_sales.settings")
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 import notifications.routing
 from notifications.auth import JwtAuthMiddleware
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "erp_sales.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
